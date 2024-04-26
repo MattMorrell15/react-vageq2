87782f88d91bbbae771c55470cd56929047ec5db
MattMorrell15 <morrel18@msu.edu>
2024-04-26 16:37:32 -0400

    added pic and css changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 import React from "react";
 import "./style.css";
 
+const magazines = [
+  {id: 1, title: 'A', theme: 'arch', isAvailable: true },
+  {id: 2 title: 'D', theme: 'arch', isAvailable: true },
+  {id: 3 title: 'CA', theme: 'design', isAvailable: false },
+];
+
+const book = {
+  title: 'AFTA',
+  author: 'EH',
+  published: '1929',
+  image: src/lasersharknew.jpg
+  width: '264',
+  height: '378',
+};
+
+function Bookshelf() {
+  return (
+    <>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      <img 
+        className="bookCover"
+        src={book.image}
+        alt={book.title + ' cover'}
+        style={{
+          width: book.width
+          height: book.height
+        }}
+        />
+      </>
+  )
+}
+
+function MagicButton () {
+  return (
+    <>
+      <h3>This is a magic button</h3>
+      <button>Magic</button>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <div>
